bb06ea36a6590462546d84710dd1d6b682ec4005
Ivan eone666 Avdeyev <eone.ukg@gmail.comv>
2020-06-20 16:28:00 +0600

    mobile support

--- src/components/movie-list.jsx
+++ src/components/movie-list.jsx
@@ -4,14 +4,24 @@ import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
     root: {
-        padding: "20px"
+        padding: "20px",
+
+
     },
     list: {
+        margin: 0,
+        padding: 0,
         display: "grid",
-        gridTemplateColumns: "repeat(10, 5fr);",
+        gridTemplateColumns: "repeat(10, 1fr);",
         gridGap: "20px",
         listStyle: "none",
-        marginBottom: "20px"
+        marginBottom: "20px",
+        [theme.breakpoints.down('md')]: {
+            gridTemplateColumns: "repeat(5,1fr)"
+        },
+        [theme.breakpoints.down('sm')]: {
+            gridTemplateColumns: "repeat(2,1fr)"
+        }
     }
 }))
 
